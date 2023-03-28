aa9821a9b859267d1c837c60d92458d3956699da
mikmet <mikko.metsaranta@orangit.fi>
2023-03-28 09:43:49 +0000

    Add stock data with mock data file

--- src/frontend/src/components/Graph.tsx
+++ src/frontend/src/components/Graph.tsx
@@ -1,16 +1,47 @@
 import Highcharts from "highcharts/highstock";
 import HighchartsReact from "highcharts-react-official";
+import data from "../data/mock.json";
+import DataService from "../service/DataService";
 
 const Graph = () => {
   const options = {
     title: {
       text: "My chart",
     },
+    rangeSelector: {
+      selected: 1,
+    },
     series: [
       {
-        data: [1, 2, 1, 4, 3, 6, 7, 3, 8, 6, 9],
+        name: "AAPL Stock Price",
+        data: DataService.processAplhaVantageStockData(data),
+        type: "area",
+        threshold: null,
+        tooltip: {
+          valueDecimals: 2,
+        },
       },
     ],
+    responsive: {
+      rules: [
+        {
+          condition: {
+            maxWidth: 500,
+          },
+          chartOptions: {
+            chart: {
+              height: 300,
+            },
+            subtitle: {
+              text: null,
+            },
+            navigator: {
+              enabled: false,
+            },
+          },
+        },
+      ],
+    },
   };
 
   return (

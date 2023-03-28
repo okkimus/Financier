c73ff728ee24e9d5f3cbaac64e56bf3d0e97714d
mikmet <mikko.metsaranta@orangit.fi>
2023-03-28 09:22:43 +0000

    Add tests for when datapoints exist

--- src/frontend/src/service/DataService.ts
+++ src/frontend/src/service/DataService.ts
@@ -1,7 +1,22 @@
+import ApiDailyStockDatapoint from "../types/ApiDailyStockDatapoint";
 import StockDatapoint from "../types/StockDatapoint";
 
 const processAplhaVantageStockData = (data: any): Array<StockDatapoint> => {
-  return [];
+  const datapoints = data["Time Series (Daily)"];
+  const dataKeys = Object.keys(datapoints);
+
+  const stockDatapoints = dataKeys.map((k) =>
+    apiDatapointToStockDatapoint(datapoints[k], k)
+  );
+
+  return stockDatapoints;
+};
+
+const apiDatapointToStockDatapoint = (
+  datapoint: ApiDailyStockDatapoint,
+  date: string
+): StockDatapoint => {
+  return [new Date(date).valueOf(), parseFloat(datapoint["4. close"])];
 };
 
 const DataService = { processAplhaVantageStockData };
--- src/frontend/src/tests/services/Dataservice.test.ts
+++ src/frontend/src/tests/services/Dataservice.test.ts
@@ -17,5 +17,64 @@ describe("DataService", () => {
       const actual = sut(emptyData);
       expect(actual).toStrictEqual([]);
     });
+
+    it("should return list with one datapoint when one datapoint exists", () => {
+      const data = {
+        "Time Series (Daily)": {
+          "2023-03-27": {
+            "1. open": "159.94",
+            "2. high": "160.77",
+            "3. low": "157.87",
+            "4. close": "158.28",
+            "5. adjusted close": "158.28",
+            "6. volume": "52390266",
+            "7. dividend amount": "0.0000",
+            "8. split coefficient": "1.0",
+          },
+        },
+      };
+
+      const actual = sut(data);
+      expect(actual).toHaveLength(1);
+      const datapoint = actual[0];
+      expect(datapoint[0]).toBe(1679875200000);
+      expect(datapoint[1]).toBe(158.28);
+    });
+
+    it("should return list with two datapoints when two datapoints exist", () => {
+      const data = {
+        "Time Series (Daily)": {
+          "2023-03-27": {
+            "1. open": "159.94",
+            "2. high": "160.77",
+            "3. low": "157.87",
+            "4. close": "158.28",
+            "5. adjusted close": "158.28",
+            "6. volume": "52390266",
+            "7. dividend amount": "0.0000",
+            "8. split coefficient": "1.0",
+          },
+          "2023-03-28": {
+            "1. open": "158.28",
+            "2. high": "161.77",
+            "3. low": "159.70",
+            "4. close": "161.28",
+            "5. adjusted close": "161.28",
+            "6. volume": "52390255",
+            "7. dividend amount": "0.0000",
+            "8. split coefficient": "1.0",
+          },
+        },
+      };
+
+      const actual = sut(data);
+      expect(actual).toHaveLength(2);
+      const datapoint1 = actual[0];
+      expect(datapoint1[0]).toBe(1679875200000);
+      expect(datapoint1[1]).toBe(158.28);
+      const datapoint2 = actual[1];
+      expect(datapoint2[0]).toBe(1679961600000);
+      expect(datapoint2[1]).toBe(161.28);
+    });
   });
 });
